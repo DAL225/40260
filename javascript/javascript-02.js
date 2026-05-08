bbb9a995c7e336c629b9d91d0e03275aed5642cb
DAL225 <255dal@gmail.com>
2026-05-08 11:18:25 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -31,21 +31,72 @@ function operacion(e){
     console.log(e.target.innerText)
     if (e.target.innerText == "+")
         suma()
+    if (e.target.innerText == "-")
+        resta()
+    if (e.target.innerText == "*")
+        multiplicacion()
+    if (e.target.innerText == "/")
+        division()
+    if (e.target.innerText == "C")
+        limpiar()
     if (e.target.innerText == "=")
         igual()
 }
 
 let var1
 let var2
+let operacionActual
 
 function suma(){
     var1 = resultado.value
     resultado.value = null
     var2 = resultado.value
     console.log(var1+var2)
+    operacionActual = "+"
+    console.log(operacionActual)
+}
+
+function resta(){
+    var1 = resultado.value
+    resultado.value = null
+    var2 = resultado.value
+    console.log(var1-var2)
+    operacionActual = "-"
+    console.log(operacionActual)
+}
+
+function multiplicacion(){
+    var1 = resultado.value
+    resultado.value = null
+    var2 = resultado.value
+    console.log(var1*var2)
+    operacionActual = "*"
+    console.log(operacionActual)
+}
+
+function division(){
+    var1 = resultado.value
+    resultado.value = null
+    var2 = resultado.value
+    console.log(var1/var2)
+    operacionActual = "/"
+    console.log(operacionActual)
 }
 
 function igual(){
     var2 = resultado.value
-    resultado.value = parseInt(var1) + parseInt(var2)
+    if (operacionActual == "+")
+        resultado.value = parseInt(var1) + parseInt(var2)
+    if (operacionActual == "-")
+        resultado.value = parseInt(var1) - parseInt(var2)
+    if (operacionActual == "*")
+        resultado.value = parseInt(var1) * parseInt(var2)
+    if (operacionActual == "/")
+        resultado.value = parseInt(var1) / parseInt(var2)
+}
+
+function limpiar(){
+    var1 = null
+    var2 = null
+    resultado.value = null
 }
